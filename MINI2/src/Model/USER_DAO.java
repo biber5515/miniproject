353a17f97de4cbce784e58c4349eb7c2b5cb24b5
Jong jae Park <66498240+pjj186@users.noreply.github.com>
2022-01-18 17:43:59 +0900

    RANK SCORE 갱신

--- MINI2/src/Model/USER_DAO.java
+++ MINI2/src/Model/USER_DAO.java
@@ -10,167 +10,219 @@ import java.util.ArrayList;
 public class USER_DAO {
 	Connection conn = null;
 	PreparedStatement pst = null;
-	ResultSet rs=null;
-	
+	ResultSet rs = null;
 
-	//데이터베이스 연결
-public void connect() {
-		
+	// 데이터베이스 연결
+	public void connect() {
+
 		try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-		String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524";
-		String user = "campus_d_3_0115";
-		String password = "smhrd3";
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524";
+			String user = "campus_d_3_0115";
+			String password = "smhrd3";
 
-		conn = DriverManager.getConnection(url, user, password);
-		}catch(Exception e) {
-			e.printStackTrace();	
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-}
+	}
+
 // db 디스커넥트
-public void close() {
-	try {
-		if(rs!=null) {
-		rs.close();
-		}
-		if(pst!=null) {
-		pst.close();
-		}
-		if(conn!=null) {
-		conn.close();
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pst != null) {
+				pst.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
-	} catch (SQLException e) {
-		e.printStackTrace();
 	}
-	
-}
+
 // db에 회원가입 정보 넣기
-public boolean insertId(String ID, int PASSWORD, String NAME) {
+	public boolean insertId(String ID, int PASSWORD, String NAME) {
 
-	boolean check = false;
-	try {
-		connect();
-		
-		String sql = "insert into user_info(ID,PASSWORD,NAME) values( ?, ?, ?)";
+		boolean check = false;
+		try {
+			connect();
 
+			String sql = "insert into user_info(ID,PASSWORD,NAME) values( ?, ?, ?)";
 
-		pst = conn.prepareStatement(sql);
+			pst = conn.prepareStatement(sql);
 
-		pst.setString(1, ID);
-		pst.setInt(2, PASSWORD);
-		pst.setString(3, NAME);
+			pst.setString(1, ID);
+			pst.setInt(2, PASSWORD);
+			pst.setString(3, NAME);
 
-		int cnt = pst.executeUpdate();
+			int cnt = pst.executeUpdate();
 
-		if (cnt > 0) { // 추가성공
-			check = true;
-		} else {// 추가 실패
-			check = false;
+			if (cnt > 0) { // 추가성공
+				check = true;
+			} else {// 추가 실패
+				check = false;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
 		}
 
-	} catch (Exception e) {
-		e.printStackTrace();
-	} finally {
-		close();
+		return check;
+
 	}
 
-	return check;
-
-}
 //입력한 로그인 하기 위해 아이디의 비번 가져오기
-public USER_VO selectOneID(String ID) {
+	public USER_VO selectOneID(String ID) {
 
-	USER_VO vo = null;
-	try {
-		connect();
+		USER_VO vo = null;
+		try {
+			connect();
 
-		String sql = "select ID,PASSWORD from USER_INFO where ID= ?";
+			String sql = "select ID,PASSWORD from USER_INFO where ID= ?";
 
-		pst = conn.prepareStatement(sql);
+			pst = conn.prepareStatement(sql);
 
-		pst.setString(1, ID);
+			pst.setString(1, ID);
 
-		rs = pst.executeQuery();
+			rs = pst.executeQuery();
 
-		// sql문 실행시 where조건절으로 특정한 번호를 가진 학생만 검색
-		// ->resultest 상에 해당 번호가 table 상에 존재하면 1명에 대한 데이터만 존재
+			// sql문 실행시 where조건절으로 특정한 번호를 가진 학생만 검색
+			// ->resultest 상에 해당 번호가 table 상에 존재하면 1명에 대한 데이터만 존재
 
-		if (rs.next()) {
-			String id = rs.getString("ID");// 현재 커서가 가르키고 있는 행의 첫뻔째 컬럼값을 읽어오겠다!
-			int password  = rs.getInt("PASSWORD");
-			vo = new USER_VO(id, password);
+			if (rs.next()) {
+				String id = rs.getString("ID");// 현재 커서가 가르키고 있는 행의 첫뻔째 컬럼값을 읽어오겠다!
+				int password = rs.getInt("PASSWORD");
+				vo = new USER_VO(id, password);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
 		}
-
-	} catch (Exception e) {
-		e.printStackTrace();
-	} finally {
-		close();
+		return vo;
 	}
-	return vo;
-}
 
 //탑 10랭크 확인하기
-public ArrayList rankCheck() {
+	public ArrayList<USER_VO> rankCheck() {
+		ArrayList<USER_VO> al = new ArrayList<USER_VO>();
+		try {
+			connect();
 
-	
-	ArrayList<USER_VO> al=new ArrayList<USER_VO>();
-	try {
-		connect();
+			String sql = "select ROWNUM,ID,NAME,SCORE from (SELECT ID,NAME,SCORE from USER_INFO order by Score desc) where ROWNUM<=10";
 
-		String sql = "select ROWNUM,ID,NAME,SCORE from (SELECT ID,NAME,SCORE from USER_INFO order by Score desc) where ROWNUM<=10";
+			pst = conn.prepareStatement(sql);
 
-		pst = conn.prepareStatement(sql);
+			rs = pst.executeQuery();
 
-		rs = pst.executeQuery();
+			// sql문 실행시 where조건절으로 특정한 번호를 가진 학생만 검색
+			// ->resultest 상에 해당 번호가 table 상에 존재하면 1명에 대한 데이터만 존재
 
-		// sql문 실행시 where조건절으로 특정한 번호를 가진 학생만 검색
-		// ->resultest 상에 해당 번호가 table 상에 존재하면 1명에 대한 데이터만 존재
+			while (rs.next()) {
+				String ID = rs.getString("ID");// 현재 커서가 가르키고 있는 행의 첫뻔째 컬럼값을 읽어오겠다!
+				String NAME = rs.getString("NAME");
+				int SCORE = rs.getInt("SCORE");
+				al.add(new USER_VO(ID, NAME, SCORE));
+			}
 
-		while(rs.next()) {
-			String ID = rs.getString("ID");// 현재 커서가 가르키고 있는 행의 첫뻔째 컬럼값을 읽어오겠다!
-			String NAME  = rs.getString("NAME");
-			int SCORE = rs.getInt("SCORE");
-			al.add(new USER_VO(ID,NAME,SCORE));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
 		}
-
-	} catch (Exception e) {
-		e.printStackTrace();
-	} finally {
-		close();
+		return al;
 	}
-	return al;
-}
 
 //선택한 아이디 랭크 확인하기
-public USER_VO selectRank(String setID) {
+	public USER_VO selectRank(String setID) {
 
-	USER_VO vo = null;
-	try {
-		connect();
+		USER_VO vo = null;
+		try {
+			connect();
 
-		String sql = "select ID,NAME,SCORE from USER_INFO where ID=?";
+			String sql = "select ID,NAME,SCORE from USER_INFO where ID=?";
 
-		pst = conn.prepareStatement(sql);
+			pst = conn.prepareStatement(sql);
 
-		pst.setString(1, setID);
+			pst.setString(1, setID);
 
-		rs = pst.executeQuery();
+			rs = pst.executeQuery();
 
-		if (rs.next()) {
-			String ID = rs.getString("ID");
-			String NAME = rs.getString("NAME");
-			int SCORE = rs.getInt("SCORE");
-			vo = new USER_VO(ID,NAME,SCORE);
+			if (rs.next()) {
+				String ID = rs.getString("ID");
+				String NAME = rs.getString("NAME");
+				int SCORE = rs.getInt("SCORE");
+				vo = new USER_VO(ID, NAME, SCORE);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
 		}
-
-	} catch (Exception e) {
-		e.printStackTrace();
-	} finally {
-		close();
+		return vo;
 	}
-	return vo;
-}
-}
 
+	// 점수 업데이트
+	
+	public int currentScore(String ID) {
+		// 특정 유저의 현재 점수를 반환
+		int currScore = 0;
+		
+		try {
+			connect();
+			String sql = "select score from user_info where id = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, ID);
+			
+			rs = pst.executeQuery();
+			
+			if(rs.next()) {
+				currScore = rs.getInt("score");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return currScore;
+	}
+	
+	public boolean updateScore(String ID, int score) {
+		// score : 증가할 점수 (안타, 홈런)
+		
+		int currScore = currentScore(ID);
+		boolean check = false;
+		
+		try {
+			connect();
+			String sql = "update user_info set score = ? where id = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, currScore + score);
+			pst.setString(2, ID);
+			
+			int cnt = pst.executeUpdate();
+			
+			if(cnt > 0) {
+				check = true; // 업데이트 체크
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return check;
+	}
+}

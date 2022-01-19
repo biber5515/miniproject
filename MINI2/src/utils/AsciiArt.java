df0e714b2bdd2594c4f12bc517ea08188459a1d5
Jong jae Park <66498240+pjj186@users.noreply.github.com>
2022-01-19 17:26:07 +0900

    아스키 아트

---
+++ MINI2/src/utils/AsciiArt.java
@@ -0,0 +1,37 @@
+package utils;
+
+public class AsciiArt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 스트라이크
+		System.out.println("   ___    _____    ___     ___    _  __    ___   ");
+		System.out.println("  / __|  |_   _|  | _ \\   |_ _|  | |/ /   | __|  ");
+		System.out.println("  \\__ \\    | |    |   /    | |   | ' <    | _|   ");
+		System.out.println("  |___/   _|_|_   |_|_\\   |___|  |_|\\_\\   |___|  ");
+		System.out.println("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| ");
+		System.out.println("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'");
+		
+		System.out.println();
+		System.out.println();
+		
+		// 안타
+		System.out.println("   ___     ___      ___    ___    _____  __   __");
+		System.out.println("  / __|   /   \\    | __|  | __|  |_   _| \\ \\ / /");
+		System.out.println("  \\__ \\   | - |    | _|   | _|     | |    \\ V /  ");
+		System.out.println("  |___/   |_|_|   _|_|_   |___|   _|_|_   _|_|_  ");
+		System.out.println("_|\"\"\"\"\"|_|\"\"\"\"\"|_| \"\"\" |_|\"\"\"\"\"|_|\"\"\"\"\"|_| \"\"\" | ");
+		System.out.println("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'");
+		
+		// 홈런
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("  _  _     ___   __  __    ___     ___    _   _   _  _   ");
+		System.out.println(" | || |   / _ \\ |  \\/  |  | __|   | _ \\  | | | | | \\| |  ");
+		System.out.println(" | __ |  | (_) || |\\/| |  | _|    |   /  | |_| | | .` |  ");
+		System.out.println(" |_||_|   \\___/ |_|__|_|  |___|   |_|_\\   \\___/  |_|\\_|  ");
+		System.out.println("_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| ");
+		System.out.println("\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'");
+	}
+}
